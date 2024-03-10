339b38311f519d044ef3138fe6e307576dca93ec
Yeqing Xia <144754603+yeqingxia@users.noreply.github.com>
2024-03-10 15:12:18 -0400

    dict: add help icon and alertDialog

--- app/src/main/java/org/algonquin/cst2355/finalproject/dictionary/DictionaryActivity.java
+++ app/src/main/java/org/algonquin/cst2355/finalproject/dictionary/DictionaryActivity.java
@@ -1,8 +1,13 @@
 package org.algonquin.cst2355.finalproject.dictionary;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.algonquin.cst2355.finalproject.R;
 
@@ -13,4 +18,24 @@ public class DictionaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dictionary, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.help) {
+            //show help alert dialog
+            new AlertDialog.Builder(this)
+                    .setTitle("Help")
+                    .setMessage("This is a dictionary app. You can search for a word and get its definition.")
+                    .show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
