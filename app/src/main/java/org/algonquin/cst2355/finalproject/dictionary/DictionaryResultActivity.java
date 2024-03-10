13eeaf055861d41d3a5d0cdbbeae11bb506585e9
Yeqing Xia <144754603+yeqingxia@users.noreply.github.com>
2024-03-10 18:52:54 -0400

    dict: add volley query word api

--- app/src/main/java/org/algonquin/cst2355/finalproject/dictionary/DictionaryResultActivity.java
+++ app/src/main/java/org/algonquin/cst2355/finalproject/dictionary/DictionaryResultActivity.java
@@ -1,14 +1,22 @@
 package org.algonquin.cst2355.finalproject.dictionary;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.algonquin.cst2355.finalproject.databinding.ActivityDictionaryResultBinding;
+import org.json.JSONArray;
 
 
 public class DictionaryResultActivity extends AppCompatActivity {
@@ -34,5 +42,25 @@ public class DictionaryResultActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setTitle(word);
         }
+        searchForTerm(word);
     }
+
+    private void searchForTerm(String term) {
+        String url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + term;
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                binding.wordTextView.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Handle error
+            }
+        });
+        queue.add(jsonArrayRequest);
+    }
+
 }
